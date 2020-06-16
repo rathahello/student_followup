fc6df0451fba9fcd184af8a41f7b4fcb70d776a5
sereyratha.chann <sereyrotha.chann@student.passerellesnumeriques.org>
2020-06-16 20:57:00 +0700

    create migrate file

---
+++ database/migrations/2020_06_16_082744_create_users_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('role')->default(0);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->unsignedBigInteger('position_id');
+            $table->foreign('position_id')
+                    ->references('id')
+                    ->on('positions');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        DB::table('users')->insert(
+            array(
+                'firstname' => 'admin',
+                'lastname' => 'user',
+                'role' => 1,
+                'email' => 'admin@example.com',
+                'position_id' => 1,
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10)
+            )
+        );
+        DB::table('users')->insert(
+            array(
+                'firstname' => 'normal',
+                'lastname' => 'user',
+                'role' => 0,
+                'email' => 'normal@example.com',
+                'position_id' => 4,
+                'password' => bcrypt('password'),
+                'remember_token' => Str::random(10)
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
